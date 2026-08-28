7ca340cc36c47c57b424a4901fd8d7a83e42077d
Workflow E2E Test <workflow-e2e@example.com>
2026-08-28 08:08:52 +0200

    chore(workflow): 54e8fc54-44e4-4b7f-b772-907085f1331d changes

--- tests/test_todos.py
+++ tests/test_todos.py
@@ -82,10 +82,23 @@ class TestTodos:
         assert test_todo.completed_at is None
 
     def test_delete_todo(self, authenticated_client, test_todo, db_session):
-        """Test deleting a todo."""
+        """Test deleting a todo updates the sidebar incomplete count via OOB."""
+        remaining_todo = Todo(
+            list_id=test_todo.list_id,
+            title="Remaining Todo",
+            position=1,
+        )
+        db_session.add(remaining_todo)
+        db_session.commit()
+
         todo_id = test_todo.id
         response = authenticated_client.delete(f"/api/todos/{todo_id}")
         assert response.status_code == 200
+        assert (
+            f'id="list-{test_todo.list_id}-count" hx-swap-oob="true">1'
+            .encode()
+            in response.content
+        )
 
         # Verify deleted
         deleted = db_session.query(Todo).filter(Todo.id == todo_id).first()
