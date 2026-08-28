ed517528314a514434ba6b9597da109171c3f933
Workflow E2E Test <workflow-e2e@example.com>
2026-08-28 08:12:55 +0200

    workflow: prepare publish

--- tests/test_todos.py
+++ tests/test_todos.py
@@ -94,11 +94,8 @@ class TestTodos:
         todo_id = test_todo.id
         response = authenticated_client.delete(f"/api/todos/{todo_id}")
         assert response.status_code == 200
-        assert (
-            f'id="list-{test_todo.list_id}-count" hx-swap-oob="true">1'
-            .encode()
-            in response.content
-        )
+        expected_count = f'id="list-{test_todo.list_id}-count" hx-swap-oob="true">1'
+        assert expected_count in response.text
 
         # Verify deleted
         deleted = db_session.query(Todo).filter(Todo.id == todo_id).first()
